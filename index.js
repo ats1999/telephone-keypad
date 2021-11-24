c035ae38c25e80960118bfde3ea158ec668f0da3
ats1999 <rkt82224@gmail.com>
2021-11-24 16:06:13 +0530

    done

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-const singleClickValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, "*", 0, "#"];
-const dblClickValues = [
-  [".", ",", "!"],
-  ["a", "b", "c"],
-  ["d", "e", "f"],
-  ["g", "h", "i"],
-  ["j", "k", "l"],
-  ["m", "n", "o"],
-  ["p", "q", "r", "s"],
-  ["t", "u", "v"],
-  ["w", "x", "y", "x"],
+// the value of each button, when clicked
+// it is indexed 0
+const clickValues = [
+  [1, ".", ",", "!"],
+  [2, "a", "b", "c"],
+  [3, "d", "e", "f"],
+  [4, "g", "h", "i"],
+  [5, "j", "k", "l"],
+  [6, "m", "n", "o"],
+  [7, "p", "q", "r", "s"],
+  [8, "t", "u", "v"],
+  [9, "w", "x", "y", "x"],
   ["*"],
   ["0"],
   ["#"],
 ];
 
-function clickHandler(btn, btnIdx) {
+/**
+ * Handle click event. Depending on the number of click, insert values in input element.
+ * @param {Object} btn HTML Button element
+ * @param {Object} input HTML Input element
+ * @param {Number} btnIdx the index of button element on page
+ */
+function clickHandler(btn, btnIdx, input) {
+  // count continues click
   let clickCount = 0;
   let timeOut;
+
   btn.addEventListener("click", (e) => {
+    // timeOut hold id of previous setTimeout
+    // if the gap between first click and second click is less than 300ms,
+    // then it can be treated as multi click
     if (timeOut) clearTimeout(timeOut);
 
     clickCount++;
 
+    // run the code after 300ms
+    // if there is another click within 300ms,
+    // then do not execute this code
     timeOut = setTimeout(() => {
-      if (clickCount == 1) {
-        console.log(singleClickValues[btnIdx]);
-      } else {
-        console.log({
-          clickCount,
-          len: dblClickValues[btnIdx].length,
-          mod: (clickCount - 1) % dblClickValues[btnIdx].length,
-        });
-        console.log(
-          dblClickValues[btnIdx][
-            ((clickCount - 1) % dblClickValues[btnIdx].length) - 1
-          ]
-        );
-        clickCount = 0;
-      }
+      const btnValues = clickValues[btnIdx];
+      const text = btnValues[(clickCount - 1) % btnValues.length];
+      input.value += text;
+      clickCount = 0;
     }, 300);
   });
 }
 window.onload = function () {
+  // get all buttons present on the page
   const btns = document.getElementsByClassName("btn");
+  const input = document.getElementById("input");
+
   for (let btn = 0; btn < btns.length; btn++) {
-    clickHandler(btns[btn], btn);
+    // assign a click handler to each button
+    clickHandler(btns[btn], btn, input);
   }
 };
